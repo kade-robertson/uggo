adc9c3d66e2fabf86b28da47da19bfc3afed9b4d
Kade Robertson <kade@kaderobertson.dev>
2023-10-31 16:42:43 -0400

    fix(proxy): customizable cache size

--- ugg-proxy/src/config.rs
+++ ugg-proxy/src/config.rs
@@ -4,6 +4,7 @@ pub struct Config {
     pub bind_address: String,
     pub bind_port: u16,
     pub log_level: String,
+    pub cache_size: u64,
 }
 
 pub fn get_config() -> Config {
@@ -11,5 +12,6 @@ pub fn get_config() -> Config {
         bind_address: var("BIND_ADDRESS").map_or_else(|_| "127.0.0.1".to_string(), |s| s),
         bind_port: var("BIND_PORT").map_or(3000, |s| s.parse::<u16>().map_or(3000, |p| p)),
         log_level: var("LOG_LEVEL").map_or_else(|_| "info".to_string(), |s| s),
+        cache_size: var("CACHE_SIZE").map_or(1000, |s| s.parse::<u64>().map_or(1000, |p| p)),
     }
 }
